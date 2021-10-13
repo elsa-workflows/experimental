6ac8a4bc1dadb84c5b05e56e3d9f2a4397ac11a3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2021-10-13 12:58:18 +0200

    Handle root statements without requiring initial block statement

--- src/dsl/Elsa.Dsl/Interpreters/WorkflowDefinitionBuilderInterpreter/VisitFile.cs
+++ src/dsl/Elsa.Dsl/Interpreters/WorkflowDefinitionBuilderInterpreter/VisitFile.cs
@@ -8,17 +8,30 @@ namespace Elsa.Dsl.Interpreters
     {
         public override IWorkflowDefinitionBuilder VisitFile(ElsaParser.FileContext context)
         {
+            var stats = context.stat();
+            var rootSequence = new Sequence();
+
+            // Push a sequence to allow multiple activities declared in sequence.
+            _containerStack.Push(rootSequence);
+
+            // Interpret child nodes.
             VisitChildren(context);
 
-            var objectStats = context.stat().OfType<ElsaParser.ObjectStatContext>().ToList();
-            var blockStats = context.stat().OfType<ElsaParser.BlockStatContext>().ToList();
-            var activities = objectStats.Select(x => (IActivity)_object.Get(x)!).ToList();
-            activities.AddRange(blockStats.Select(x => (Sequence)_expressionValue.Get(x)!));
-            
-            var rootActivity = activities.Count == 1 ? activities.Single() : new Sequence(activities.ToArray());
+            // Extract activities from child nodes.
+            var activities = stats.Select(x => _expressionValue.Get(x) as IActivity).Where(x => x != null).Select(x => x!).ToList();
 
-            _workflowDefinitionBuilder.WithRoot(rootActivity);
-            
+            if (activities.Count == 1)
+            {
+                // We only have one activity, so we can use it as the root and discard the root sequence.
+                _workflowDefinitionBuilder.WithRoot(activities.Single());
+            }
+            else
+            {
+                // Assign the collected child activities to the root sequence.
+                rootSequence.Activities = activities.ToList();
+                _workflowDefinitionBuilder.WithRoot(rootSequence);
+            }
+
             return DefaultResult;
         }
     }
